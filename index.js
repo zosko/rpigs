854e9177504a68f9a9d5083fdbd37f217c82bc9e
Bosko <bosko.petreski@gmail.com>
2021-03-09 23:07:53 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,252 @@
+var http = require('http');
+var express = require('express');
+const bodyParser = require("body-parser");
+var path = require("path");
+const SerialPort = require('serialport');
+const ByteLength = require('@serialport/parser-byte-length');
+const app = express();
+
+app.use('/',express.static(path.join(__dirname, './')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+http.createServer(app).listen(8080);
+
+app.get('/', function (req, res) {
+	res.sendFile(path.join(__dirname + '/index.html'));  
+})
+app.post('/connect', function (req, res) {
+	const port = new SerialPort(req.body.port, { baudRate: 57600 });
+	const parser = port.pipe(new ByteLength({length: 9}));
+
+	port.on("open", () => {
+	  console.log('serial port open: ' + req.body.port);
+	});
+	port.on('error', function(err) {
+	  console.log('Error: ', err.message)
+	})
+	parser.on('data', data =>{
+	  process_incoming_bytes(data);
+	});
+
+	setTimeout(function() {
+		port.write('AT+PIN000000\r\n');
+    }, 1000);
+	setTimeout(function() {
+		port.write('AT+CONA4C249839091C\r\n');
+    }, 2000);
+	
+	res.send("");
+})
+app.get('/ports', function (req, res) {
+	SerialPort.list().then(function(ports){
+  		res.send(JSON.stringify(ports));
+	});
+})
+app.get('/flight', function (req, res) {
+	var flight = {
+		lat:lat,
+		lng:lng,
+		alt:alt,
+		gps_sats:gps_sats,
+		distance:distance,
+		speed:speed,
+		voltage:voltage,
+		rssi:rssi,
+		current:current,
+		heading:heading,
+		arm:getArmed(),
+		stabilization:getStabilization(),
+		fuel:fuel,
+		roll:roll,
+		pitch:pitch,
+	}
+  	res.send(JSON.stringify(flight));
+})
+
+var lat = 0.0
+var lng = 0.0
+var alt = 0
+var gps_sats = 0
+var distance = 0
+var speed = 0
+var voltage = 0.0
+var rssi = 0
+var current = 0
+var heading = 0
+var flight_mode = 0
+var fuel = 0
+var roll = 0
+var pitch = 0
+
+const State = {
+    IDLE: 0,
+    DATA: 1,
+    XOR: 2
+}
+
+const PACKET_SIZE = 0x09
+const START_BYTE  = 0x7E
+const DATA_START  = 0x10
+const DATA_STUFF  = 0x7D
+const STUFF_MASK  = 0x20
+
+const VFAS_SENSOR  = 0x0210
+const CELL_SENSOR  = 0x0910
+const VSPEED_SENSOR  = 0x0110
+const GSPEED_SENSOR  = 0x0830
+const ALT_SENSOR  = 0x0100
+const GALT_SENSOR  = 0x0820
+const DISTANCE_SENSOR  = 0x0420
+const FUEL_SENSOR  = 0x0600
+const GPS_SENSOR  = 0x0800
+const CURRENT_SENSOR  = 0x200
+const HEADING_SENSOR  = 0x0840
+const RSSI_SENSOR  = 0xF101
+const FLYMODE_SENSOR  = 0x0400
+const GPS_STATE_SENSOR  = 0x0410
+const PITCH_SENSOR  = 0x0430
+const ROLL_SENSOR  = 0x0440
+const AIRSPEED_SENSOR  = 0x0A00
+const FLIGHT_PATH_VECTOR  = 0x0450
+const RX_BAT = 0xF104
+
+var state = State.IDLE
+var bufferIndex = 0
+var buffer = Buffer.alloc(9);
+var newLatitude = false
+var newLongitude = false
+var latitude = 0.0
+var longitude = 0.0
+
+//MARK: Functions
+    function getStabilization(){
+        var mode = flight_mode / 10 % 10
+        if (mode == 2){
+            return "horizon"
+        }
+        else if (mode == 1) {
+            return "angle"
+        }
+        else{
+            return "manual"
+        }
+    }
+    function getArmed(){
+        var mode = flight_mode % 10
+        if (mode == 5) {
+            return "YES"
+        }
+        return "NO"
+    }
+    
+    //MARK: Helpers
+    function buffer_get_int16(buffer,index){
+        return buffer[index] << 8 | buffer[index - 1]
+    }
+    function buffer_get_int32(buffer, index) {
+        return buffer[index] << 24 | buffer[index - 1] << 16 | buffer[index - 2] << 8 | buffer[index - 3]
+    }
+    
+    //MARK: Telemetry functions
+    function process_incoming_bytes(incomingData){
+    	const data = Buffer.from(incomingData);
+        for (var i = 0; i < data.length; i++) {
+            switch (state) {
+            case State.IDLE:
+                if (data[i] == START_BYTE) {
+                    state = State.DATA
+                    bufferIndex = 0
+                }
+                break
+            case State.DATA:
+                if (data[i] == DATA_STUFF) {
+                    state = State.XOR
+                }
+                else if (data[i] == START_BYTE) {
+                    bufferIndex = 0
+                }
+                else{
+                    buffer[bufferIndex] = data[i]
+                    bufferIndex += 1
+                }
+                break
+            case State.XOR:
+                buffer[bufferIndex] = data[i] ^ STUFF_MASK
+                bufferIndex += 1
+                state = State.DATA
+                break
+            }
+            
+            if (bufferIndex == PACKET_SIZE) {
+                state = State.IDLE
+                
+                var _ = buffer[0] //sensor type
+                var packetType = buffer[1]
+                if (packetType == DATA_START) {
+                    var dataType = buffer_get_int16(buffer,3)
+                    var rawData = buffer_get_int32(buffer,7)
+                    
+                    switch (dataType) {
+                    case VFAS_SENSOR:
+                        voltage = parseFloat(rawData) / 100.0
+                        break
+                    case GSPEED_SENSOR:
+                        speed = parseInt((parseFloat(rawData) / (1944.0 / 100.0)) / 27.778)
+                        break
+                    case GALT_SENSOR:
+                        alt = parseInt(parseFloat(rawData) / 100.0)
+                        break
+                    case DISTANCE_SENSOR:
+                        distance = parseInt(rawData)
+                        break
+                    case FUEL_SENSOR:
+                        fuel = parseInt(rawData)
+                        break
+                    case GPS_SENSOR:
+                        var gpsData = parseFloat((rawData & 0x3FFFFFFF)) / 10000.0 / 60.0
+                        if (rawData & 0x40000000 > 0) {
+                            gpsData = -gpsData
+                        }
+                        if (parseInt(rawData) & parseInt(0x80000000) == 0) {
+                            newLatitude = true
+                            latitude = gpsData
+                        } else {
+                            newLongitude = true
+                            longitude = gpsData
+                        }
+                        if (newLatitude && newLongitude) {
+                            newLongitude = false
+                            newLatitude = false
+                            lat = latitude
+                            lng = longitude
+                        }
+                        break
+                    case CURRENT_SENSOR:
+                        current = parseInt(parseFloat(rawData) / 10.0)
+                        break
+                    case HEADING_SENSOR:
+                        heading = parseInt(parseFloat(rawData) / 100.0)
+                        break
+                    case RSSI_SENSOR:
+                        rssi = parseInt(rawData)
+                        break
+                    case FLYMODE_SENSOR:
+                        flight_mode = parseInt(rawData)
+                        break
+                    case GPS_STATE_SENSOR:
+                        gps_sats = parseInt(rawData % 100)
+                        break
+                    case PITCH_SENSOR:
+                        pitch = parseInt(parseFloat(rawData) / 10.0)
+                        break
+                    case ROLL_SENSOR:
+                        roll = parseInt(parseFloat(rawData) / 10.0)
+                        break
+                    default:
+                        break
+                    }
+                }
+            }
+        }
+    }
